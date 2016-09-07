dde49bbd569fe810c5e668f2365dffa7d8905f67
Tijndagamer <tijndagamer25@gmail.com>
2016-09-07 21:09:29 +0200

    Minor formatting change in example

--- ADXL335/examples/ADXL335_example/ADXL335_example.ino
+++ ADXL335/examples/ADXL335_example/ADXL335_example.ino
@@ -11,14 +11,17 @@ ADXL335 adxl335(A0, A1, A2, 3.3);
 
 void setup()
 {
-    Serial.begin(9600); 
+    Serial.begin(9600);
 }
 
 void loop()
 {
     Serial.print(adxl335.readX());
+    Serial.print("\t");
     Serial.print(adxl335.readY());
+    Serial.print("\t");
     Serial.print(adxl335.readZ());
+    Serial.print("\t");
     Serial.println();
     delay(500); 
 }
